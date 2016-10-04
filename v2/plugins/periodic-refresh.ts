2cbeea239c5f31d1ae5a692da051cc13c2c235c2
Dave Ingram <dave@dmi.me.uk>
2016-10-03 21:19:57 -0700

    Rewrite periodic-refresh plugin to be kind of smart; writing to its refreshMinutes now sets the timer automatically

--- v2/plugins/periodic-refresh.ts
+++ v2/plugins/periodic-refresh.ts
@@ -1,14 +1,34 @@
-namespace periodicRefresh {
+class periodicRefresh {
 
-  export function wakeup() : void {
+  static readonly DEFAULT_REFRESH_MINUTES = 60;
+
+  private static _refreshMinutes: number = 0;
+  private static _interval: number | null = null;
+
+  static wakeup() : void {
     console.log('periodicRefresh: timer fired - leaving idle mode');
-    //iitc.util.idleReset();
+    iitc.idle.reset();
   }
 
-  export const refreshMinutes = 60;
+  static get refreshMinutes() {
+    return periodicRefresh._refreshMinutes;
+  }
 
-  window.setInterval(wakeup, refreshMinutes * 60 * 1000);
-
+  static set refreshMinutes(minutes: number) {
+    periodicRefresh._refreshMinutes = minutes;
+    if (periodicRefresh._interval) {
+      window.clearInterval(periodicRefresh._interval);
+    }
+    if (minutes > 0) {
+      console.warn('Periodic refresh will occur every ' + minutes + ' minutes');
+      periodicRefresh._interval = window.setInterval(periodicRefresh.wakeup, minutes * 60 * 1000);
+    } else if (periodicRefresh._interval) {
+      console.warn('Stopped periodic refresh timer');
+      periodicRefresh._interval = null;
+    }
+  }
 }
 
-(window.plugin || (window.plugin = {})).periodicRefresh = periodicRefresh;
+periodicRefresh.refreshMinutes = periodicRefresh.DEFAULT_REFRESH_MINUTES;
+
+plugin.periodicRefresh = periodicRefresh;
